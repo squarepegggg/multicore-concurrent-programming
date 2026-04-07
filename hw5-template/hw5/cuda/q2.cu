8da71f8f1e8192859a1abeb0bd3824140b706fba
squarepegggg <matthewolan@utexas.edu>
2026-04-07 17:24:08 -0500

    half of problem 2

--- hw5-template/hw5/cuda/q2.cu
+++ hw5-template/hw5/cuda/q2.cu
@@ -1,4 +1,12 @@
 
+#include <__clang_cuda_runtime_wrapper.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+
+
 int main(int argc, char **argv)
 {
     // Implement your solution for question 2. The input file is inp.txt
@@ -7,5 +15,24 @@ int main(int argc, char **argv)
     //  (1) q2a.txt which contains the minimum value in the input array
     //  (2) q2b.txt which contains an array B (in the same format as inp.txt)
     //      where B[i] = the last digit of A[i]
+
+
+    // read from inp.txt
+    FILE *file = fopen("inp.txt","r");
+    if(!file){
+        printf("Failed to Open file");
+    }
+
+    // Count integers
+    int n = 0, val;
+    char sep;
+    while (fscanf(file, "%d%c", &val, &sep) == 2) n++;
+    rewind(file);
+    int *h_A = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) fscanf(file, "%d%c", &h_A[i], &sep);
+    fclose(file);
+
+
+
     return 0;
 }
